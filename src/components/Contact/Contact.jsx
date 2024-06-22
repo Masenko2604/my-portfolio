a63bc5ddbe90b9db42807c67cb36b294f3442b50
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-22 12:05:13 +0200

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,17 +10,29 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl('contact/email.png')} alt="Email icon" />
+          <img
+            src={getImageUrl('contact/email.png')}
+            className={styles.img}
+            alt="Email icon"
+          />
           <a href="mailto:masyenko@gmail.com">masyenko@gmail.com</a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl('contact/linkedin.png')} alt="LinkedIn icon" />
+          <img
+            src={getImageUrl('contact/linkedin.png')}
+            className={styles.img}
+            alt="LinkedIn icon"
+          />
           <a href="https://www.linkedin.com/in/tetyana-masyenko-3533942a5/">
             Linkedin
           </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl('contact/git.png')} alt="Github icon" />
+          <img
+            src={getImageUrl('contact/git.png')}
+            className={styles.img}
+            alt="Github icon"
+          />
           <a href="https://github.com/Masenko2604">GitHub</a>
         </li>
       </ul>
